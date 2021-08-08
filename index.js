cdbb17726d904fce70d64a4b4dd3138bf4499789
Daniel Williams <danielpaulwill@gmail.com>
2021-08-08 18:48:30 -0400

    commit tree 8214e038d60922e7a98c680d5d42feb2d9b5d125

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ]
 
+/*
+
+
+  
+  function superbowlWin(array) {
+    array.find(function(element){
+//        let noWin = undefined
+        if (element.result === 'W') {
+            console.log(element.year, "test");
+            return element.year;
+        }//   else return noWin
+
+    })
+    }
+
+
+    function superbowlWin(array) {
+        let a = array.find(ele => ele.result === "W")
+        return a.year
+      }
+
+          
+   */
+
+function superbowlWin(array) {
+    let a = array.find(element => element.result === "W");
+    if (a != undefined) {
+        return a.year
+    }
+}
+
+
+
+
+    
+
+
+/*
+    function superbowlWin(array) {
+        array.find(element => {
+            return element
+        })
+    }
+    */
